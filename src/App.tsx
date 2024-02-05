b7540bdf4b8cce16daab7816dd583779552b917d
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2024-02-05 09:33:43 +0100

    Changed the headlines of the page

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Made by Marius</h1>
+      <h2>Hello World!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
